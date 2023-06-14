fdfc48a2c04d318edaa9a225ac90ae50a591e3fb
andreilivshits <andreilivshits@gmail.com>
2023-06-14 18:38:13 +0000

    commint

--- src/routes/(app)/+page.js
+++ src/routes/(app)/+page.js
@@ -1,8 +1,12 @@
 import { redirect } from '@sveltejs/kit';
 
 export const load = async ({ parent }) => {
-	const { session } = await parent();
+	const { session, supabase } = await parent();
 	if (!session) {
 		throw redirect(303, '/login');
+	} else {
+		const id = session.user.id;
+		const { data, error } = await supabase.from('profiles').select('last_loc').eq('id', id).single();
+		throw redirect(303, data['last_loc']);
 	}
 };
